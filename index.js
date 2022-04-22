ebabafa6c09210f258a6520cf790ec1b6d704d59
Bijay Sharma <yoursbijay@outlook.com>
2022-04-22 13:25:08 +0530

    Database Connectivity

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 import express from 'express';
+import connectDB from './config/db.js'
+import {errorHandler, notFound} from './middleware/errorMiddleware.js';
+import stateRoutes from './routes/stateRoutes.js';
 
 const app = express();
 app.use(express.json());
+connectDB();
 
 app.use("/", (req, res) => {
     res.send("Hello from the server");
 });
+
+
+//routes
+app.use('/api/states', stateRoutes);
+
+// Handlers
+app.use(notFound);
+app.use(errorHandler);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`The Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
